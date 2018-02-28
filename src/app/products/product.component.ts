bd1a230706bc4b891fcd8e5c37171994e130e731
AAKASH HANDA <AAKASH HANDA>
2018-02-28 08:20:12 +0530

    A

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -5,4 +5,47 @@ import {Component} from '@angular/core';
     templateUrl:'./product.component.html'
 })
 
-export class ProductComponent{}
+export class ProductComponent{
+    title:string="***Product List***";
+    showHide=true;
+    ShowImage=false;
+    products:any[]=[
+        {  
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        },
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        }
+    ]
+
+    toggleImage():void{
+        this.ShowImage = !this.ShowImage
+    }
+}
+
+
+
+/*
+string
+number
+boolean
+One Way Binding
+--Data Binding{{}}
+--Property Binding[]
+--Event Binding()
+Two Way Binding[()]
+*/
